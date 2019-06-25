67d5d56586b404d6fc4fe48aad997529de2619ff
Mahkii900 <col.graff.battleschool@gmail.com>
2019-06-25 13:57:53 -0600

    First Real Commit

--- practice.js
+++ practice.js
@@ -1,7 +1,7 @@
 //////////////////PROBLEM 1////////////////////
 
 //Create a variable called myName that is a string data type
-
+var myName = 'Daniel';
 //Code here
 
 //////////////////PROBLEM 2////////////////////
@@ -9,25 +9,25 @@
 //Create a variable called myAge that is a number data type
 
 //Code here
-
+var myAge = 22;
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
 //Code here
-
+var lovesCode = true;
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
 //Code here
-
+var greatestFear;
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
 //Code here
-
+var devMountainGoal = null;
 //////////////////PROBLEM 6////////////////////
 
 //Create a function declaration called greeting that
@@ -36,21 +36,25 @@
 //plus the value of the name parameter.
 
 //Code here
-
+function greeting (name) {
+  return "Hello, " + name;
+}
 //////////////////PROBLEM 7////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
 //Code Here
-
+var newGreeting = function(name){
+  return "Hello, " + name;
+}
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
-
+var groceries = ["apples", "milk", "eggs", "bread"];
 //////////////////PROBLEM 9////////////////////
 
 //Create an object saved to the variable dog.
@@ -59,12 +63,17 @@
 //and goodBoy (a boolean).
 
 //Code Here
-
+var dog = {
+  name: "Karl Marx II",
+  color: "Blood-red",
+  age: 500,
+  goodBoy: false
+}
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
 //Code Here
-
+devMountainClassPet = dog.name;
 //////////////////PROBLEM 10////////////////////
 
 //Write a function called nameCheck that takes in a name parameter.
@@ -75,7 +84,15 @@
 // with NAMEPARAM being the name parameter being passed in
 
 // Code here
-
+function nameCheck(NAMEPARAM){
+  if (NAMEPARAM == 'Steven'){
+    return 'What is up Steven?';
+  } else if (NAMEPARAM == 'Bryan'){
+    return 'Hey Bryan!';
+  } else {
+    return 'Cool name, ' + NAMEPARAM;
+  }
+}
 //////////////////PROBLEM 11////////////////////
 
 // Create a function called add that takes in two parameters
@@ -83,7 +100,9 @@
 // The add function should return the two parameters added together
 
 //Code Here
-
+function add(num1, num2) {
+  
+}
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
